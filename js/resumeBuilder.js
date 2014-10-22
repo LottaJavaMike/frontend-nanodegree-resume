3e043fd8e6c6a6c5cb63a458eaeb8004c6cf69da
Mike Shea <emtshea@gmail.com>
2014-10-22 06:42:35 -0400

    Add objects to resumebuilder

---
+++ js/resumeBuilder.js
@@ -0,0 +1,84 @@
+var bio = {
+	"name" : "Michael Shea",
+	"role" : "Front-End Developer",
+	"contacts" : {
+		"phone": "321-555-1212",
+		"email": "emtshea@gmail.com",
+		"location": "Orlando, Florida",
+		"twitter" : "@MikeInVa",
+		"github" : "LottaJavaMike"
+	},
+	"welcome message" : "welcome to whatever this is",
+	"picture url" : "url to image later",
+	"Skills": ["networking", "server administration", "powershell", "programming"]
+}
+
+var education = {
+	"schools" : [
+		{
+			"name" : "ECPI Universitiy - College of Technology",
+			"location" : "Virginia Beach, VA",
+			"degree" : "Associate of Science",
+			"major" : "Computer and Information Sciences",
+			"graduated": "December 2009 ",
+			"url" : "http://www.ecpi.edu/technology/"
+			
+		},
+		{
+			"name" : "Gibbs Long Island",
+			"location" : "Melville, New York",
+			"degree" : "Associate of Occupational Studies",
+			"major" : "Computer Network Operations",
+			"graduated" : "December 2004",
+			"url" : "None"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title" : "Intro to HTML & CSS",
+			"school" : "Udacity",
+			"dates" : "2014",
+			"url" : "http://www.udacity.com/course/ud304"
+		},
+		{
+			"title" : "JavaScript Basics",
+			"school" : "Udacity",
+			"dates" : "2014",
+			"url" : "http://www.udacity.com/course/ud804"
+		}
+	]
+}
+
+var work = {
+	"jobs" : [
+		{
+			"employer" : "Bright House Networks",
+			"title" : "Computer Support Specialist II",
+			"location" : "Orlando, FL",
+			"dates" : "May 2012 - Present",
+			"description" : "rovide hardware and software support for 3000+ PCs in an environment consisting of Windows XP, Windows 7 and Apple workstations. Hardware support includes (but not limited to) desktops, notebooks and tablets from HP, Dell and Panasonic. Software supported includes (but not limited to) products from Microsoft, Mozilla, Winzip, Adobe, BMC, Symantec, etc."
+		},
+		{
+			"employer" : "4 Corner Resources",
+			"title" : "Contractor - Technical Services",
+			"location" : "Orlando, FL",
+			"dates" : "April 2011 – May 2012",
+			"description" : "Technical Services Contractor at Bright House Networks"
+		}
+	]
+}
+
+var projects = {
+	"projects" : [
+		{
+			"title" : "Project A",
+			"dates worked" : "Jan 2010 - June 2012",
+			"description" : "description of whatever Project A would be",
+			"images" : [
+				"url to image 1" , "url to image 2"
+			] //urls to screenshots of projects being worked on
+		}
+	]
+}
+
+
